e3fc41344db4c1cadf5bab3aa3654fbcebee6f0d
Cristhian Felipe <cristhianfelipeyt@gmail.com>
2024-02-10 12:00:11 -0300

    hehe

--- src/App.tsx
+++ src/App.tsx
@@ -16,16 +16,19 @@ import {
   WindowHeader,
   NumberInput,
   Avatar,
+  TextInput,
 } from 'react95';
 import original from 'react95/dist/themes/original';
 import { TypeAnimation } from 'react-type-animation';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { nightOwl } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { Chrono } from 'react-chrono';
+
+// Images
 import tikTk from './assets/tikTk.png';
 import cepSearch from './assets/cepSearch.png'
 import pokedex from './assets/pokedex.png';
 import rjTecnologia from './assets/rjTecnologia.png'
-
 
 export default function App() {
   const [activeTabProjects, setActiveTabProjects] = useState({ activeTab: 'TikTok Downloader' });
@@ -40,6 +43,16 @@ export default function App() {
   const pokedexCodeBlock = 'A Pokedex é uma aplicação web desenvolvida com HTML, CSS e JavaScript, que utiliza uma API pública de Pokemon para exibir informações abrangentes sobre diferentes Pokémon. Os recursos incluem uma interface visualmente atraente, busca por nome ou número de Pokémon, exibição de detalhes como tipo, habilidades e estatísticas. Com uma navegação intuitiva, proporciona aos usuários uma experiência envolvente e informativa ao explorar o vasto mundo dos Pokémon.';
   const cepSearchCodeBlock = 'O "CEP Search" é uma aplicação React que permite aos usuários buscar informações detalhadas utilizando CEPs. Integrando-se a uma API pública, oferece uma experiência simples e intuitiva. Os principais recursos incluem busca rápida, exibição de detalhes do endereço e uma interface amigável, tornando a obtenção de informações de CEP fácil e eficiente.';
 
+  const items = [
+    {
+      title: "March 2023",
+      cardTitle: "CAMERITE",
+      cardSubtitle: "Software Developer",
+      cardDetailedText: `
+      Ingressei na Camerite em março de 2023 como desenvolvedor de software, focando em tecnologias como Angular, React e JavaScript para o desenvolvimento de interfaces web responsivas. Além disso, explorei o universo do desenvolvimento mobile usando React Native. Destaquei-me na gestão eficiente de bancos de dados relacionais utilizando MYSQL e Sequelize, aprimorando a modelagem e implementação de esquemas. A cultura colaborativa da empresa e o incentivo ao aprendizado contínuo contribuíram significativamente para meu crescimento profissional.
+      `,
+    }
+  ];
 
   return (
     <>
@@ -51,11 +64,11 @@ export default function App() {
             </p>
             <Toolbar>
               <Handle size={35} />
-              <a href="#languages">
+              {/* <a href="#languages">
                 <Button variant='menu'>
                   Languages
                 </Button>
-              </a>
+              </a> */}
               <a href="#projects">
 
                 <Button variant='menu'>
@@ -131,7 +144,7 @@ export default function App() {
                 }
                 {
                   activeTabProjects.activeTab === 'Rj Tecnologia' &&
-                  <a href="https://rj-tecnologia-website-h56qt87js-nosycolg.vercel.app/" target='_blank'>
+                  <a href="https://rj-tecnologia-website.vercel.app/" target='_blank'>
                     <Avatar square size={'100%'} className='p-1 cursor-pointer'>
                       <img src={rjTecnologia} />
                     </Avatar>
@@ -157,7 +170,7 @@ export default function App() {
                     <SyntaxHighlighter
                       language="python"
                       style={nightOwl}
-                      customStyle={{ fontSize: 20, borderRadius: 10 }}
+                      customStyle={{ fontSize: 20, borderRadius: 10, overflow: 'hidden' }}
                       wrapLongLines
                     >
                       {tiTkSaverCodeBlock}
@@ -166,7 +179,9 @@ export default function App() {
                       <Avatar square size={70} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg' />
                       <Avatar square size={70} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg' style={{ backgroundColor: '#282B30' }} />
                     </div>
-                    <Button fullWidth>Acess Github Project</Button>
+                    <a href="https://github.com/tiktksaver" target='_blank'>
+                      <Button fullWidth>Acess Github Project</Button>
+                    </a>
                   </div>
                 }
                 {
@@ -175,7 +190,7 @@ export default function App() {
                     <SyntaxHighlighter
                       language="python"
                       style={nightOwl}
-                      customStyle={{ fontSize: 20, borderRadius: 10 }}
+                      customStyle={{ fontSize: 20, borderRadius: 10, overflow: 'hidden' }}
                       wrapLongLines
                     >
                       {cepSearchCodeBlock}
@@ -184,7 +199,9 @@ export default function App() {
                       <Avatar square size={70} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg' />
                       <Avatar square size={70} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg' style={{ backgroundColor: '#282B30' }} />
                     </div>
-                    <Button fullWidth>Acess Github Project</Button>
+                    <a href="https://github.com/nosycolg/cep-search" target='_blank'>
+                      <Button fullWidth>Acess Github Project</Button>
+                    </a>
                   </div>
                 }
                 {
@@ -193,7 +210,7 @@ export default function App() {
                     <SyntaxHighlighter
                       language="python"
                       style={nightOwl}
-                      customStyle={{ fontSize: 20, borderRadius: 10 }}
+                      customStyle={{ fontSize: 20, borderRadius: 10, overflow: 'hidden' }}
                       wrapLongLines
                     >
                       {rjTecnologiaCodeBlock}
@@ -202,7 +219,9 @@ export default function App() {
                       <Avatar square size={70} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg' />
                       <Avatar square size={70} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg' style={{ backgroundColor: '#282B30' }} />
                     </div>
-                    <Button fullWidth>Acess Github Project</Button>
+                    <a href="https://github.com/nosycolg/rj-tecnologia-website" target='_blank'>
+                      <Button fullWidth>Acess Github Project</Button>
+                    </a>
                   </div>
                 }
                 {
@@ -211,7 +230,7 @@ export default function App() {
                     <SyntaxHighlighter
                       language="python"
                       style={nightOwl}
-                      customStyle={{ fontSize: 20, borderRadius: 10 }}
+                      customStyle={{ fontSize: 20, borderRadius: 10, overflow: 'hidden' }}
                       wrapLongLines
                     >
                       {pokedexCodeBlock}
@@ -221,7 +240,9 @@ export default function App() {
                       <Avatar square size={70} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg' />
                       <Avatar square size={70} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg' />
                     </div>
-                    <Button fullWidth>Acess Github Project</Button>
+                    <a href="https://github.com/nosycolg/pokedex" target='_blank'>
+                      <Button fullWidth>Acess Github Project</Button>
+                    </a>
                   </div>
                 }
               </GroupBox>
@@ -238,31 +259,77 @@ export default function App() {
                   <Tab value={'1'}>Experiences</Tab>
                   <Tab value={'2'}>Contact</Tab>
                 </Tabs>
-                <TabBody style={{ height: 300 }}>
+                <TabBody>
                   {about === '0' && (
                     <div>
-                      <GroupBox label='Order:'>
-                        <div style={{ padding: '0.5em 0 0.5em 0' }}>Amount:</div>
-                        <NumberInput width='100%' min={0} defaultValue={0} />
-                        <br />
-                        <Checkbox
-                          name='shipping'
-                          value='fast'
-                          label='Fast shipping'
-                          onChange={() => null}
-                          defaultChecked
-                        />
-                      </GroupBox>
+                      <WindowContent>
+                        <GroupBox label='Apresentation' style={{ fontSize: 22 }}>
+                        Olá, meu nome é Cristhian Felipe da Silva, tenho 18 anos e sou um autodidata dedicado ao aprendizado desde os 16. Nasci em São Paulo e cresci em Recife, Pernambuco.
+
+Minha jornada no mundo do conhecimento começou cedo, quando decidi assumir as rédeas da minha educação aos 16 anos. O autodidatismo tornou-se não apenas um método, mas uma filosofia de vida para mim. Essa abordagem me permitiu explorar áreas que genuinamente me interessam, desenvolvendo habilidades práticas e conhecimentos que considero fundamentais.
+
+Atualmente, mantenho meu foco em aprimorar habilidades relacionadas à tecnologia e programação, uma paixão que cresceu naturalmente ao longo dos anos. Estou sempre em busca de desafios que me permitam expandir meu conhecimento e contribuir para projetos significativos.
+
+Embora minha trajetória seja breve, cada passo tem sido marcado por um compromisso inabalável com o aprendizado contínuo. Estou ansioso para explorar novas oportunidades e desafios, aproveitando ao máximo cada experiência que a vida e a aprendizagem autodidata têm a oferecer.
+                        </GroupBox>
+                      </WindowContent>
                     </div>
                   )}
                   {about === '1' && (
                     <div>
-                      <div>Coming soon</div>
+                      <Chrono
+                        items={items}
+                        mode="HORIZONTAL"
+                        itemWidth={1920}
+                        showSingle
+                        flipLayout
+                        timelinePointShape="square"
+                        theme={{
+                          primary: 'black',
+                          secondary: '#060084',
+                          cardBgColor: 'white',
+                          titleColor: 'white',
+                          titleColorActive: 'white',
+                        }}
+
+                      />
                     </div>
                   )}
                   {about === '2' && (
-                    <div>
-                      <div>Coming soon</div>
+                    <div style={{ display: 'flex', flexDirection: 'row', gap: 30 }}>
+                      <Avatar square size={600}>
+                      </Avatar>
+                      <div className='w-full'>
+                        <p style={{ lineHeight: 1.3, fontSize: 20, padding: 5 }}>
+                          Your Name
+                        </p>
+                        <TextInput
+                          value={''}
+                          placeholder='Example'
+                          fullWidth
+                        />
+                        <p style={{ lineHeight: 1.3, fontSize: 20, padding: 5 }}>
+                          Your E-mail
+                        </p>
+                        <TextInput
+                          value={''}
+                          placeholder='example@example.com...'
+                          fullWidth
+                        />
+                        <p style={{ lineHeight: 1.3, fontSize: 20, padding: 5 }}>
+                          Message
+                        </p>
+                        <TextInput
+                          multiline
+                          value={''}
+                          placeholder=''
+                          fullWidth
+                          rows={8}
+                        />
+                        <a href="https://github.com/nosycolg/pokedex" target='_blank'>
+                          <Button fullWidth className='mt-2'>Send</Button>
+                        </a>
+                      </div>
                     </div>
                   )}
                 </TabBody>
